db0b7f2fb1a05b7e780accea5a34ffed7b4c794a
Tamas Kecskes <tkecskes@isis.vanderbilt.edu>
2016-02-08 12:47:48 -0600

    slight error correction

--- src/client/views/play.jsx
+++ src/client/views/play.jsx
@@ -204,8 +204,8 @@ export default class PlayView extends React.Component {
             (board[3] !== '_' && board[3] === board[4] && board[3] === board[5]) ||
             (board[6] !== '_' && board[6] === board[7] && board[6] === board[8]) ||
             (board[0] !== '_' && board[0] === board[3] && board[0] === board[6]) ||
-            (board[1] !== '_' && board[1] === board[4] && board[1] === board[6]) ||
-            (board[2] !== '_' && board[2] === board[5] && board[2] === board[7]) ||
+            (board[1] !== '_' && board[1] === board[4] && board[1] === board[7]) ||
+            (board[2] !== '_' && board[2] === board[5] && board[2] === board[8]) ||
             (board[0] !== '_' && board[0] === board[4] && board[0] === board[8]) ||
             (board[2] !== '_' && board[2] === board[4] && board[2] === board[6])) {
             return true;
